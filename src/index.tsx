e8cd04b1bde6d3dd7a032e9c5f757c199b7d9b92
isaac <isaac.kps@gmail.com>
2020-10-29 15:09:04 +0800

    fix: exposing overlay content

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+export * from "./common/context/OverlayContext";
 export * from "./components/AddressBook";
 export * from "./components/AddressResolver/AddressResolver";
 export * from "./components/NetworkBar";
@@ -5,4 +6,4 @@ export * from "./components/UI/Button";
 export * from "./components/UI/Input";
 export * from "./components/UI/Loader";
 export * from "./components/UI/Overlay";
-export * from "./common/context/OverlayContext";
+export * from "./components/UI/Overlay/OverlayContent";
