d18aa80fa893476f3780eb64cf71eb3e057bf396
isaac <isaac.kps@gmail.com>
2020-11-11 10:56:43 +0800

    feat: updated test cases for address book local

--- src/components/AddressBook/AddressBookLocal/AddressBookLocal.test.tsx
+++ src/components/AddressBook/AddressBookLocal/AddressBookLocal.test.tsx
@@ -15,35 +15,47 @@ const addressBook = {
   "0x1d350495b4c2a51fbf1c9dedadeab288250c703e": "China Oil",
   "0x28f7ab32c521d13f2e6980d072ca7ca493020145": "Standard Chartered",
 };
+const filteredLocalAddresses = [
+  "0xe94e4f16ad40adc90c29dc85b42f1213e034947c",
+  "0xa61b056da0084a5f391ec137583073096880c2e3",
+  "0x1d350495b4c2a51fbf1c9dedadeab288250c703e",
+  "0x28f7ab32c521d13f2e6980d072ca7ca493020145",
+];
 
 describe("addressBookLocal", () => {
   it("should show default message", () => {
     mockUseAddressBook.mockReturnValue({ addressBook: {} });
-    render(<AddressBookLocal onAddressSelect={() => {}} searchTerm={""} network="local" />);
+    render(<AddressBookLocal onAddressSelect={() => {}} filteredLocalAddresses={[]} network="local" />);
     expect(screen.getByText("No address found. Try importing a csv template file?")).not.toBeNull();
   });
 
   it("should show correct result when address is searched", () => {
     mockUseAddressBook.mockReturnValue({ addressBook: addressBook });
-    render(<AddressBookLocal onAddressSelect={() => {}} searchTerm={""} network="local" />);
+    render(
+      <AddressBookLocal onAddressSelect={() => {}} filteredLocalAddresses={filteredLocalAddresses} network="local" />
+    );
     expect(screen.getByText("DBS")).not.toBeNull();
   });
 
   it("should show correct result when identifier is searched", () => {
     mockUseAddressBook.mockReturnValue({ addressBook: addressBook });
-    render(<AddressBookLocal onAddressSelect={() => {}} searchTerm={""} network="local" />);
+    render(
+      <AddressBookLocal onAddressSelect={() => {}} filteredLocalAddresses={filteredLocalAddresses} network="local" />
+    );
     expect(screen.getByText("0x28f7ab32c521d13f2e6980d072ca7ca493020145")).not.toBeNull();
   });
 
   it("should show more than 1 result", () => {
     mockUseAddressBook.mockReturnValue({ addressBook: addressBook });
-    render(<AddressBookLocal onAddressSelect={() => {}} searchTerm={""} network="local" />);
+    render(
+      <AddressBookLocal onAddressSelect={() => {}} filteredLocalAddresses={filteredLocalAddresses} network="local" />
+    );
     expect(screen.getAllByTestId("table-row")).toHaveLength(4);
   });
 
   it("should show no result found message", () => {
     mockUseAddressBook.mockReturnValue({ addressBook: addressBook });
-    render(<AddressBookLocal onAddressSelect={() => {}} searchTerm={"abc"} network="local" />);
+    render(<AddressBookLocal onAddressSelect={() => {}} filteredLocalAddresses={[]} network="local" />);
     expect(screen.getByText("No address found.")).not.toBeNull();
   });
 });
