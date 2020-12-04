9b364aeb04c8ea07138aa9939941752508dde63c
Sim Boon Long <simboonlong@gmail.com>
2020-12-04 15:14:35 +0800

    fix: rm redundant props

--- src/components/UI/Pagination/Pagination.stories.tsx
+++ src/components/UI/Pagination/Pagination.stories.tsx
@@ -12,44 +12,17 @@ export default {
 
 export const Default = (): ReactElement => {
   const [currentPage, setCurrentPage] = useState(1);
-  return (
-    <Pagination
-      onPageClick={() => {
-        console.log("page changed!!");
-      }}
-      totalNoOfPages={3}
-      currentPage={currentPage}
-      setCurrentPage={setCurrentPage}
-    />
-  );
+  return <Pagination totalNoOfPages={3} currentPage={currentPage} setCurrentPage={setCurrentPage} />;
 };
 
 export const PaginationNumberSelected = (): ReactElement => {
   const [currentPage, setCurrentPage] = useState(1);
 
-  return (
-    <PaginationNumber
-      pageNumber={1}
-      currentPage={currentPage}
-      setCurrentPage={setCurrentPage}
-      onPageClick={(i: number) => {
-        console.log(`page ${i} changed!!`);
-      }}
-    />
-  );
+  return <PaginationNumber pageNumber={1} currentPage={currentPage} setCurrentPage={setCurrentPage} />;
 };
 
 export const PaginationNumberUnselected = (): ReactElement => {
   const [currentPage, setCurrentPage] = useState(2);
 
-  return (
-    <PaginationNumber
-      pageNumber={1}
-      currentPage={currentPage}
-      setCurrentPage={setCurrentPage}
-      onPageClick={(i: number) => {
-        console.log(`page ${i} changed!!`);
-      }}
-    />
-  );
+  return <PaginationNumber pageNumber={1} currentPage={currentPage} setCurrentPage={setCurrentPage} />;
 };
