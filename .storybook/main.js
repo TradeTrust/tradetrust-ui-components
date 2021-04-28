4571ca03b3fa41f7003e31f40b1d6d0e8e6997df
Sim Boon Long <simboonlong@gmail.com>
2021-04-28 16:32:44 +0800

    feat: webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,9 @@ const path = require("path");
 const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
+  core: {
+    builder: "webpack5", // https://gist.github.com/shilman/8856ea1786dcd247139b47b270912324#installation
+  },
   stories: ["../src/**/*.stories.tsx"],
   addons: ["@storybook/addon-essentials", "@storybook/addon-postcss"],
   webpackFinal: (config) => {
