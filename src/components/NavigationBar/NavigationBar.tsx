ef5c9e50f3cebb1727c252bcfe0468ec2f265abc
Sim Boon Long <simboonlong@gmail.com>
2021-05-24 16:29:01 +0800

    fix: navbar redesign (#67)

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -9,40 +9,22 @@ export interface NavigationBarProps {
   toggleNavBar: boolean;
 }
 
-const NavigationBarStyle = styled.nav`
-  button {
-    color: #6e787f;
+export const NavigationBarStyled = styled.nav`
+  // try to avoid scoped styles here as much as possible
+  a {
     font-size: 16px;
-    font-family: "Roboto";
   }
 
-  a {
-    color: #6e787f;
-    display: block;
-    font-size: 16px;
-    font-family: "Roboto";
+  .dropdown {
+    a {
+      font-size: 14px;
+    }
   }
 
   .navbar-toggler {
-    padding: 0.25rem 0.75rem;
-    background-color: transparent;
-    border: 1px solid transparent;
-    border-radius: 0.25rem;
-
-    &:hover,
-    &:focus {
-      text-decoration: none;
-      outline: none;
-    }
-
-    .icon-bar {
-      background-color: #454b50;
-      display: block;
-      width: 22px;
+    .top-bar,
+    .bottom-bar {
       height: 2px;
-      border-radius: 1px;
-      transition: transform 0.2s ease-out;
-      position: absolute;
     }
 
     .top-bar {
@@ -53,97 +35,55 @@ const NavigationBarStyle = styled.nav`
       transform: translateY(0) rotate(-45deg);
     }
 
-    &.collapsed .top-bar {
-      transform: translateY(-4px) rotate(0);
+    &.collapsed {
+      .top-bar {
+        transform: translateY(-4px) rotate(0);
+      }
+
+      .bottom-bar {
+        transform: translateY(4px) rotate(0);
+      }
     }
-
-    &.collapsed .bottom-bar {
-      transform: translateY(4px) rotate(0);
-    }
-  }
-
-  .dropdown-item {
-    font-size: 14px;
-  }
-
-  .max-h-0 {
-    max-height: 0px;
-  }
-
-  .max-h-full {
-    max-height: 100vh;
-  }
-`;
-
-export const NavigationButtonStyle = styled.div`
-  .create-btn {
-    font-size: 16px;
-    color: #3b8cc5;
-    background: #ffffff;
-    border: 1px solid #e7eaec;
-    box-sizing: border-box;
-    box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.15);
-    border-radius: 12px;
-  }
-
-  .verify-btn {
-    font-size: 16px;
-    color: #ffffff;
-    background: #3b8cc5;
-    border: 2px solid #3b8cc5;
-    box-sizing: border-box;
-    box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.15);
-    border-radius: 12px;
   }
 `;
 
 export const NavigationBar: FunctionComponent<NavigationBarProps> = (props) => {
   return (
-    <NavigationBarStyle>
+    <NavigationBarStyled>
       <div className={`container py-2`}>
-        <div className="relative flex items-center justify-between h-16">
-          <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
+        <div className="relative flex items-center justify-center">
+          <div className="lg:hidden absolute left-0">
             <button
-              className={`navbar-toggler outline-none ${props.toggleNavBar ? "" : "collapsed"}`}
+              className={`navbar-toggler relative w-6 h-6 ${props.toggleNavBar ? "" : "collapsed"}`}
               onClick={() => {
                 props.setToggleNavBar(!props.toggleNavBar);
-                console.log("NavigationBar: ", props.toggleNavBar);
-                // props.onClick(!props.toggleNavBar);
               }}
             >
-              <span className="icon-bar top-bar" />
-              <span className="icon-bar bottom-bar" />
+              <span className="w-full bg-greyblue-800 transition-transform duration-200 ease-out absolute block top-bar" />
+              <span className="w-full bg-greyblue-800 transition-transform duration-200 ease-out absolute block bottom-bar" />
             </button>
           </div>
-          <div className="flex-1 flex items-center justify-center lg:items-stretch lg:justify-start">
-            <div className="flex-shrink-0">
-              <a href="https://www.tradetrust.io/">
-                <img
-                  data-testid="nav-logo-home"
-                  className="img-fluid h-12"
-                  src="https://www.tradetrust.io/static/images/tradetrust_logo.svg"
-                  alt="TradeTrust"
-                />
-              </a>
-            </div>
-            <div className="hidden lg:block md:ml-12">
-              <div className="flex h-full items-center">{props.leftMenuChildren}</div>
-            </div>
-            <div className="hidden md:block md:absolute md:right-0 lg:relative lg:ml-auto">
-              <div className="flex h-full items-center">{props.rightMenuChildren}</div>
-            </div>
-          </div>
+          <a href="https://www.tradetrust.io/">
+            <img
+              data-testid="nav-logo-home"
+              className="img-fluid h-10"
+              src="https://www.tradetrust.io/static/images/tradetrust_logo.svg"
+              alt="TradeTrust"
+            />
+          </a>
+          <div className="hidden lg:block md:ml-12">{props.leftMenuChildren}</div>
+          <div className="hidden md:block lg:ml-auto absolute right-0 lg:relative">{props.rightMenuChildren}</div>
         </div>
       </div>
       <div className={`lg:hidden`}>
         <div
-          className={`container  w-full bg-white overflow-auto  transition-height ease-in-out duration-700 h-full max-h-0 ${
-            props.toggleNavBar ? "max-h-full" : ""
+          className={`container rounded-md bg-white overflow-auto transition-height ease-in-out duration-700 ${
+            props.toggleNavBar ? "max-h-screen" : "max-h-0"
           }`}
         >
           {props.mobileMenuChildren}
         </div>
       </div>
-    </NavigationBarStyle>
+    </NavigationBarStyled>
   );
 };
