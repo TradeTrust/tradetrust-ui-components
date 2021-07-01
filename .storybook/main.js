e51a9529da29ef343ec22d91f6801483070f7d11
Sim Boon Long <simboonlong@gmail.com>
2021-07-01 15:13:25 +0800

    fix: configs reorganize (#87)

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,12 +5,15 @@ const toPath = (_path) => path.join(process.cwd(), _path);
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   addons: ["@storybook/addon-essentials", "@storybook/addon-postcss"],
+  typescript: {
+    reactDocgen: "react-docgen", // once react-docgen-typescript v2 in included in storybook, remove this config
+  },
   webpackFinal: (config) => {
     return {
       ...config,
       module: {
         ...config.module,
-        rules: custom.module.rules
+        rules: custom.module.rules,
       },
       // https://github.com/storybookjs/storybook/issues/13277#issuecomment-751747964
       resolve: {
@@ -20,8 +23,8 @@ module.exports = {
           "@emotion/core": toPath("node_modules/@emotion/react"),
           "@emotion/styled": toPath("node_modules/@emotion/styled"),
           "emotion-theming": toPath("node_modules/@emotion/react"),
-        }
-      }
+        },
+      },
     };
   },
 };
