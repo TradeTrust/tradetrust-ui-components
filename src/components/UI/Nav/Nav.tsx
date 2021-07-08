a1ef7cdf34bb5dc18c90bdd1d1facda795b867a3
ryansee <ryansee.sw@outlook.com>
2021-07-08 15:25:46 +0800

    fix: svg icon for Tile components

--- src/components/UI/Nav/Nav.tsx
+++ src/components/UI/Nav/Nav.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, SVGProps } from "react";
 import { ChevronLeft } from "react-feather";
 
 export const BackArrow: FunctionComponent = () => (
@@ -11,11 +11,17 @@ export const BackArrow: FunctionComponent = () => (
 export interface TileInfoProps {
   title: string;
   description: string;
+  Test: React.FunctionComponent<SVGProps<SVGElement>>;
 }
 
-export const TileInfo: FunctionComponent<TileInfoProps> = ({ title, description }) => (
-  <div className="p-4 w-96 h-24 font-normal rounded-xl max-w-lg bg-white shadow-xl inline-block transition-colors duration-200 hover:bg-gray-50">
-    <h3 className="font-ubuntu font-normal text-1.625 text-cerulean-200">{title}</h3>
-    <p className="text-cloud-900 mt-px">{description}</p>
+export const TileInfo: FunctionComponent<TileInfoProps> = ({ title, description, Test }) => (
+  <div className="flex p-3 w-96 h-28 font-normal rounded-xl max-w-lg bg-white shadow-xl inline-block transition-colors duration-200 hover:bg-gray-50">
+    <div className="flex flex-col w-10/12">
+      <h3 className="font-ubuntu font-normal text-1.625 text-cerulean-200">{title}</h3>
+      <p className="text-cloud-900 mt-1">{description}</p>
+    </div>
+    <div className="w-2/12 m-auto">
+      <Test />
+    </div>
   </div>
 );
