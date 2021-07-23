62c6056a179ef8772378d2b42b73186155e8ce44
ryansee <ryansee.sw@outlook.com>
2021-07-23 10:22:57 +0800

    fix: removed commented code

--- src/components/AddressResolver/AddressResolver.tsx
+++ src/components/AddressResolver/AddressResolver.tsx
@@ -8,23 +8,21 @@ export const AddressResolver: FunctionComponent = () => {
 
   return (
     <>
-      <div className="flex flex-col md:flex-row md:items-end">
-        <div className="flex">
-          <Button
-            className="bg-white text-cerulean rounded-xl shadow-lg hover:bg-gray-50"
-            onClick={() => {
-              setNewEndpoint(true);
-            }}
-            disabled={isNewEndpoint}
-          >
-            <div className="flex items-center mx-0">
-              <div className="col-auto mr-2">
-                <IconAddFile />
-              </div>
-              <div className="col-auto">Add</div>
+      <div className="flex md:justify-end">
+        <Button
+          className="bg-white text-cerulean rounded-xl shadow-lg hover:bg-gray-50"
+          onClick={() => {
+            setNewEndpoint(true);
+          }}
+          disabled={isNewEndpoint}
+        >
+          <div className="flex items-center mx-0">
+            <div className="col-auto mr-2">
+              <IconAddFile />
             </div>
-          </Button>
-        </div>
+            <div className="col-auto">Add</div>
+          </div>
+        </Button>
       </div>
       <div className="md:bg-white md:rounded-xl md:shadow-lg md:mt-6 md:p-7">
         <AddressesTable isNewEndpoint={isNewEndpoint} setNewEndpoint={setNewEndpoint} />
