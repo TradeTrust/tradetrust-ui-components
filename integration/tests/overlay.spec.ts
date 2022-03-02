2044e9251b7c4012f0e68576a6748632377c74d8
puayhiang <68947167+puayhiang@users.noreply.github.com>
2022-03-02 19:05:18 +0800

    feat/ text-overlay toggle test

---
+++ integration/tests/overlay.spec.ts
@@ -0,0 +1,20 @@
+import { Selector } from "testcafe";
+
+fixture`Text Overlay`
+  .page`http://localhost:9009/iframe.html?id=ui-overlay--text&args=&viewMode=story`;
+
+const RootSelector = Selector("#root", { visibilityCheck: true });
+const ModalSelector = Selector("#root > div.overlay", { visibilityCheck: true });
+
+const openButton = RootSelector.find("#root > button");
+const modalCloseButton = ModalSelector.find("[data-testid='overlay-close']");
+const overlayTitle = ModalSelector.find("[data-testid='overlay-title']");
+
+test("Toggle Overlay Modal", async (t) => {
+  await t.hover(openButton);
+  await t.click(openButton);
+  await t.expect(overlayTitle.exists).ok();
+  await t.hover(modalCloseButton);
+  await t.click(modalCloseButton);
+  await t.expect(overlayTitle.exists).notOk();
+});
