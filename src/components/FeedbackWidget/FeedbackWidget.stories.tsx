68a66215bb5c607e4e97ae87121042e0868e4270
Sim Boon Long <simboonlong@gmail.com>
2022-04-19 08:59:44 +0800

    feat: feedback widget (#126)

---
+++ src/components/FeedbackWidget/FeedbackWidget.stories.tsx
@@ -0,0 +1,61 @@
+import React, { FunctionComponent } from "react";
+import { FeedbackWidget } from "./FeedbackWidget";
+import { FeedbackWidgetText } from "./FeedbackWidgetText";
+
+export default {
+  title: "FeedbackWidget/FeedbackWidget",
+  component: FeedbackWidget,
+  parameters: {
+    componentSubtitle: "Feedback widget",
+  },
+};
+
+export const Default: FunctionComponent = () => {
+  return (
+    <>
+      <div style={{ width: "320px" }}>
+        <p className="mb-4">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+          aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
+          dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+          amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit
+          laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
+          voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+          pariatur.
+        </p>
+        <p className="mb-4">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+          aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
+          dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+          amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit
+          laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
+          voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+          pariatur.
+        </p>
+        <p className="mb-4">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+          aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
+          dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+          amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit
+          laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
+          voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+          pariatur.
+        </p>
+      </div>
+      <FeedbackWidget>
+        <FeedbackWidgetText
+          positiveFeedbackUrl="https://example.com/yes"
+          positiveFeedbackHandle={() => {}}
+          negativeFeedbackUrl="https://example.com/no"
+          negativeFeedbackHandle={() => {}}
+        />
+      </FeedbackWidget>
+    </>
+  );
+};
---
+++ src/components/FeedbackWidget/FeedbackWidgetText.test.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { FeedbackWidgetText } from "./FeedbackWidgetText";
+
+describe("FeedbackWidgetText", () => {
+  it("should show default feedback text", () => {
+    render(
+      <FeedbackWidgetText
+        positiveFeedbackUrl="https://example.com/yes"
+        positiveFeedbackHandle={() => {}}
+        negativeFeedbackUrl="https://example.com/no"
+        negativeFeedbackHandle={() => {}}
+      />
+    );
+
+    expect(screen.getByText("Is this page helpful?")).toBeInTheDocument();
+  });
+
+  it("should show 'yes' feedback text with 'ideas' github url", () => {
+    render(
+      <FeedbackWidgetText
+        positiveFeedbackUrl="https://example.com/yes"
+        positiveFeedbackHandle={() => {}}
+        negativeFeedbackUrl="https://example.com/no"
+        negativeFeedbackHandle={() => {}}
+      />
+    );
+    fireEvent.click(screen.getByText("Yes"));
+
+    expect(
+      screen.getByText("Thank you for your feedback. Do leave us a feedback on Github to help us do even better.")
+    ).toBeInTheDocument();
+    expect(screen.getByText("Github")).toHaveAttribute("href", "https://example.com/yes");
+  });
+
+  it("should show 'no' feedback text with 'improvements' github url", () => {
+    render(
+      <FeedbackWidgetText
+        positiveFeedbackUrl="https://example.com/yes"
+        positiveFeedbackHandle={() => {}}
+        negativeFeedbackUrl="https://example.com/no"
+        negativeFeedbackHandle={() => {}}
+      />
+    );
+    fireEvent.click(screen.getByText("No"));
+
+    expect(
+      screen.getByText("We are sorry to hear that. Do leave us a feedback on Github to help us do better.")
+    ).toBeInTheDocument();
+    expect(screen.getByText("Github")).toHaveAttribute("href", "https://example.com/no");
+  });
+});
